cf04f988ba05a5fc63d35ed7600f7550ebcb7ac1
Rafi Dwi <rafidwiriz@gmail.com>
2018-05-25 12:41:43 +0700

    Merapikan direktori lexer

--- syntatic-analysis/lexer/nltk_postag.py
+++ syntatic-analysis/lexer/nltk_postag.py
@@ -2,7 +2,7 @@ from nltk.tag import CRFTagger
 
 # Melakukan POS tag dengan menggunakan CRF Tagger NLTK dan model tagger Indonesia
 ct = CRFTagger()
-ct.set_model_file('E:/Transformers/My Book/Python/project-nlu/syntatic-analysis/lexer/nltk/all_indo_man_tag_corpus_model.crf.tagger')
+ct.set_model_file('E:/Transformers/My Book/Python/project-nlu/syntatic-analysis/lexer/postag/all_indo_man_tag_corpus_model.crf.tagger')
 
 kalimat = 'Budi membeli dua buah pisang'
 hasil = ct.tag_sents([kalimat.split()])
