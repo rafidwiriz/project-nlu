3857ba1965ae3883519c992467c2111ae118a976
Rafi Dwi <rafidwiriz@gmail.com>
2018-05-25 09:29:32 +0700

    percobaan pertama POS tag dengan spaCy

---
+++ syntatic-analysis/lexer/spacy/postag.py
@@ -0,0 +1,12 @@
+import spacy
+
+# Melakukan POS tagging dengan menggunakan model fasttext Indonesia yang telah dikonversi
+nlp = spacy.load('E:/Transformers/My Book/Python/svara_voice/model/id_model')
+doc = nlp(u'Budi menyukai pisang')
+
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+          token.shape_, token.is_alpha, token.is_stop)
+
+# Hasil: token hanya berisi informasi teks, lemma, shape, isAlpha dan isStop.
+#        Sedangkan bagian POS dan tag tidak memiliki informasi sama sekali.
