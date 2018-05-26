9161af747c1381496922e74930aa5ba9cadb70c6
Rafi Dwi <rafidwiriz@gmail.com>
2018-05-26 09:01:18 +0700

    Mencoba conll2json

--- syntatic-analysis/lexer/spacy_postag.py
+++ syntatic-analysis/lexer/spacy_postag.py
@@ -1,10 +1,16 @@
 import spacy
 
 # Melakukan POS tagging dengan menggunakan model fasttext Indonesia yang telah dikonversi
-nlp = spacy.load('E:/Transformers/My Book/Python/svara_voice/model/id_model')
+nlp = spacy.load('E:/Transformers/My Book/Python/svara_voice/model/model-final')
+nlp2 = spacy.load('en_core_web_sm')
 doc = nlp(u'Budi membeli dua buah pisang')
+doc2 = nlp2(u'John buys two bananas')
 
 for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+          token.shape_, token.is_alpha, token.is_stop)
+
+for token in doc2:
     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
           token.shape_, token.is_alpha, token.is_stop)
 
