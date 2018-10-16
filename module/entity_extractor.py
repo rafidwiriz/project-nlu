0a05d4b1d5dce34c18d398dad03d5418757c140b
Rafi Dwi <rafidwiriz@gmail.com>
2018-10-16 21:02:19 +0700

    Tambah skema IOB

--- module/entity_extractor.py
+++ module/entity_extractor.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-def entity_tag(sent_data, entities):
+def entity_tag(sent_data, entities, scheme="BILOU"):
     """SentenceData*, dict -> None"""
     slot = {}
     text = ['O']*(len(sent_data.get("text").split()))
@@ -8,15 +8,22 @@ def entity_tag(sent_data, entities):
         slot[entity["entity"]] = entity["value"]
         end = entity["start"] + len(entity["value"].split())
         for i in range(entity["start"], end):
-            if (entity["start"] == (end - 1)):
-                text[i] = "U-{}".format(entity["entity"])
-            else:
+            if (scheme == "BILOU"):
+                if (entity["start"] == (end - 1)):
+                    text[i] = "U-{}".format(entity["entity"])
+                else:
+                    if (i == entity["start"]):
+                        text[i] = "B-{}".format(entity["entity"])
+                    elif (i == (end - 1)):
+                        text[i] = "L-{}".format(entity["entity"])
+                    else:
+                        text[i] = "I-{}".format(entity["entity"])
+            elif (scheme == "IOB"):
                 if (i == entity["start"]):
                     text[i] = "B-{}".format(entity["entity"])
-                elif (i == (end - 1)):
-                    text[i] = "L-{}".format(entity["entity"])
                 else:
                     text[i] = "I-{}".format(entity["entity"])
+
     sent_data.set("entities", text)
 
 def extract_entity(sent_data, entities):
